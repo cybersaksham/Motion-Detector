299a306c6d11f109f8940eaec79b80a29dfdaec4
Saksham <2020UCP1801@gmail.com>
2021-07-17 15:46:09 +0530

    version 1.0

---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from tkinter import *
+from tkinter.filedialog import *
+
+Tk().withdraw()
+file = askopenfilename(defaultextension=".mp4", filetypes=[("All Files", "*.*")])
+
+if file == "":
+    exit()
+
+try:
+    test = cv2.VideoCapture(file)
+except Exception as e:
+    print(e)
+    exit()
+
+cap = cv2.VideoCapture(file)
+
+_, frame1 = cap.read()
+ret, frame2 = cap.read()
+
+while cap.isOpened() and ret:
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if cv2.contourArea(contour) < 900:
+            continue
+        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Motion Detector", frame1)
+
+    frame1 = frame2
+    ret, frame2 = cap.read()
+
+    if cv2.waitKey(40) == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
